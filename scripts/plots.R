f036013748dea5313fb1b7d06923a4945658d6a8
Felipe Figueiredo <philsf79@gmail.com>
2021-09-03 13:11:19 -0300

    templates aplicados

---
+++ scripts/plots.R
@@ -0,0 +1,17 @@
+# setup -------------------------------------------------------------------
+# library(ggplot2)
+# library(survminer)
+
+ff.col <- "steelblue" # good for single groups scale fill/color brewer
+ff.pal <- "Paired"    # good for binary groups scale fill/color brewer
+
+# Theme setting (less is more)
+theme_set(
+  theme_classic() +
+    theme_update(
+      legend.position = "top"
+    )
+)
+
+# plots -------------------------------------------------------------------
+
